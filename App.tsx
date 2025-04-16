a70802b2083d70b3a3f3ae6a27a3cfd27369aa99
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-16 16:17:06 -0300

    Expo dev client

--- App.tsx
+++ App.tsx
@@ -9,12 +9,15 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreat } from './notifications/notificationsTags';
 
 OneSignal.initialize("392c1cd8-b90e-4096-8e18-447390f05b6c")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  tagUserEmailCreat("nome@email.com")
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
